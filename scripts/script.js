688ff3d5903255e2b3c2035aa7d836d8db782625
zaitken <zaraaitken18@gmail.com>
2022-11-19 12:27:02 +0000

    inital commit, adds prompts, alerts and confirms

---
+++ scripts/script.js
@@ -0,0 +1,129 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  var characterLength = prompt("How many characters would you like your password to be?"); {
+    if (characterLength < 10 || characterLength > 64) {
+      alert("You must enter a password length between 10 and 64 characters");
+    }
+    var includeSpecialChar = confirm("Would you like to use special characters in your password?");
+    var includeNums = confirm("Would you like to include numbers in your password?");
+    var includeUppercase = confirm("Would you like to include uppercase letters in yourpassword?");
+    var includeLowercase = confirm("Would you like to include lowercase letters in your password?");
+   }
+    return [characterLength, includeSpecialChar, includeNums, includeUppercase, includeLowercase];
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+  var options = getPasswordOptions();
+  
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
